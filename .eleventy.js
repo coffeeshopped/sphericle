190e6a46fe22c00e1862dd854005ee32ba2f5ebe
Chadwick Wood <chadwick@coffeeshopped.com>
2022-10-05 14:51:09 -0500

    First commit

---
+++ .eleventy.js
@@ -0,0 +1,9 @@
+const eleventySass = require("eleventy-sass");
+const eleventyNavigationPlugin = require("@11ty/eleventy-navigation");
+
+module.exports = function(eleventyConfig) {
+  eleventyConfig.addPlugin(eleventySass);
+  eleventyConfig.addPlugin(eleventyNavigationPlugin);
+  
+  return { dir: { input: "_src", output: "_site" } };
+};
